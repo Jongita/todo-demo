cffb29550717e8e7726212bb6b986511fde9b626
Jongita <jurgitajonaityte@gmail.com>
2024-09-26 15:04:45 +0300

    added tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,6 +4,7 @@ const task1 = 'To do homworks';
 const task2 = 'Run 3km';
 const task3 = 'Wash dishes! & wash the floor!';
 const task4 = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, veniam iure cupiditate quo impedit consequuntur corporis sunt officia. Laborum quos debitis beatae ea autem consectetur deserunt nesciunt dolorem minus quod.';
+const newTask = 'To do homworks and read book';
 
 describe('Website on load', () => {
 
@@ -21,7 +22,7 @@ describe('Website on load', () => {
   });
 })
 
-describe('Add ToDo task and verify that task exists', () => {
+describe('Add ToDo tasks and verify that tasks exists', () => {
 
   describe('Add ToDo task as string', () => {
     beforeEach(() => {
@@ -29,22 +30,16 @@ describe('Add ToDo task and verify that task exists', () => {
     });
 
     it('should type task and verify that task exists', () => {
-      cy.get('input.new-todo').type(`${task1}{enter}`);
+      cy.addTodo(task1);
       cy.get('.todo-list').find('label').contains(task1).should('exist');
       cy.get('.todo-list li').should('have.length', 1)
-      cy.get('strong').should('contain', '0'); // Cahnged from 1 to 0
+      cy.get('strong').should('contain', '0'); // should be 1
     });
 
     it('should verify all footer elements exist', () => {
-      cy.get('.todo-count').should('exist');
-      cy.get('.todo-count strong').should('exist');
-      cy.get('.todo-count ng-pluralize').should('exist');
-      cy.get('.filters').within(() => {
-        cy.get('a').contains('All').should('exist');
-        cy.get('a').contains('active').should('exist');
-        cy.get('a').contains('Completed').should('exist');
-      });
-      cy.get('.clear-completed').should('exist');
+      cy.verifyTodoCount();
+      cy.verifyFilterLinks();
+      cy.verifyClearCompleted();
     });
   });
 
@@ -54,22 +49,16 @@ describe('Add ToDo task and verify that task exists', () => {
     });
 
     it('should type task and verify that task exists', () => {
-      cy.get('input.new-todo').type(`${task2}{enter}`);
+      cy.addTodo(task2);
       cy.get('.todo-list').find('label').contains(task2).should('exist');
       cy.get('.todo-list li').should('have.length', 1)
-      cy.get('strong').should('contain', '0'); // Cahnged from 1 to 0
+      cy.get('strong').should('contain', '0'); // should be 1
     });
 
     it('should verify all footer elements exist', () => {
-      cy.get('.todo-count').should('exist');
-      cy.get('.todo-count strong').should('exist');
-      cy.get('.todo-count ng-pluralize').should('exist');
-      cy.get('.filters').within(() => {
-        cy.get('a').contains('All').should('exist');
-        cy.get('a').contains('active').should('exist');
-        cy.get('a').contains('Completed').should('exist');
-      });
-      cy.get('.clear-completed').should('exist');
+      cy.verifyTodoCount();
+      cy.verifyFilterLinks();
+      cy.verifyClearCompleted();
     });
   });
 
@@ -79,22 +68,16 @@ describe('Add ToDo task and verify that task exists', () => {
     });
 
     it('should type task and verify that task exists', () => {
-      cy.get('input.new-todo').type(`${task3}{enter}`);
+      cy.addTodo(task3);
       cy.get('.todo-list').find('label').contains(task3).should('exist');
       cy.get('.todo-list li').should('have.length', 1)
-      cy.get('strong').should('contain', '0'); // Cahnged from 1 to 0
+      cy.get('strong').should('contain', '0'); // should be 1
     });
 
     it('should verify all footer elements exist', () => {
-      cy.get('.todo-count').should('exist');
-      cy.get('.todo-count strong').should('exist');
-      cy.get('.todo-count ng-pluralize').should('exist');
-      cy.get('.filters').within(() => {
-        cy.get('a').contains('All').should('exist');
-        cy.get('a').contains('active').should('exist');
-        cy.get('a').contains('Completed').should('exist');
-      });
-      cy.get('.clear-completed').should('exist');
+      cy.verifyTodoCount();
+      cy.verifyFilterLinks();
+      cy.verifyClearCompleted();
     });
   });
 
@@ -104,22 +87,38 @@ describe('Add ToDo task and verify that task exists', () => {
     });
 
     it('should type task and verify that task exists', () => {
-      cy.get('input.new-todo').type(`${task4}{enter}`);
+      cy.addTodo(task4);
       cy.get('.todo-list').find('label').contains(task4).should('exist');
       cy.get('.todo-list li').should('have.length', 1)
-      cy.get('strong').should('contain', '0'); // Changed from 1 to 0
+      cy.get('strong').should('contain', '0'); // should be 1
     });
 
     it('should verify all footer elements exist', () => {
-      cy.get('.todo-count').should('exist');
-      cy.get('.todo-count strong').should('exist');
-      cy.get('.todo-count ng-pluralize').should('exist');
-      cy.get('.filters').within(() => {
-        cy.get('a').contains('All').should('exist');
-        cy.get('a').contains('active').should('exist');
-        cy.get('a').contains('Completed').should('exist');
-      });
-      cy.get('.clear-completed').should('exist');
+      cy.verifyTodoCount();
+      cy.verifyFilterLinks();
+      cy.verifyClearCompleted();
+    });
+  });
+
+
+  describe('Add two ToDo tasks', () => {
+    beforeEach(() => {
+      cy.visit(pageUrl);
+    });
+
+    it('should type task and verify that task exists', () => {
+      cy.addTodo(task1);
+      cy.addTodo(task2);
+      cy.get('.todo-list').find('label').contains(task1).should('exist');
+      cy.get('.todo-list').find('label').contains(task2).should('exist');
+      cy.get('.todo-list li').should('have.length', 2)
+      cy.get('strong').should('contain', '1'); // should be 2
+    });
+
+    it('should verify all footer elements exist', () => {
+      cy.verifyTodoCount();
+      cy.verifyFilterLinks();
+      cy.verifyClearCompleted();
     });
   });
 });
@@ -130,12 +129,29 @@ describe('Mark task as completed', () => {
   });
 
   it('mark task as completed and verify how many items left', () => {
-    cy.get('input.new-todo').type(`${task1}{enter}`);
+    cy.addTodo(task1);
     cy.get('.todo-list').find('label').contains(task1).should('exist');
     cy.get('.todo-list li').should('have.length', 1)
     cy.contains('.todo-list li', task1).find('input.toggle').click()
     cy.get('strong').should('contain', '-1'); // Changed from 0 to -1
   });
+
+
+});
+
+describe('Edit a task', () => {
+  beforeEach(() => {
+    cy.visit(pageUrl);
+  });
+
+  it('should type task and possible to edit task', () => {
+    cy.addTodo(task1);
+    cy.editTask(task1, newTask)
+    cy.get('.todo-list').find('label').contains(task1).should('exist');
+    cy.get('.todo-list li').should('have.length', 1)
+    cy.get('strong').should('contain', '0'); // should be 1
+  });
+
 });
 
 describe('Remove task and verify that task does not exist', () => {
@@ -144,7 +160,7 @@ describe('Remove task and verify that task does not exist', () => {
   });
 
   it('should type task, verify that task exists, and remove the task', () => {
-    cy.get('input.new-todo').type(`${task1}{enter}`);
+    cy.addTodo(task1);
     cy.get('.todo-list').find('label').contains(task1).should('exist');
     cy.get('.todo-list').find('label').contains(task1).parents('li').trigger('mouseover');
     cy.get('button[title="TODO:REMOVE THIS EVENTUALLY"]').click({ force: true });
@@ -155,10 +171,10 @@ describe('Remove task and verify that task does not exist', () => {
 describe('Clear task works only when task is marked as completed ', () => {
   beforeEach(() => {
     cy.visit(pageUrl);
+    cy.addTodo(task1);
   });
 
   it('clear button should not clear not selected task', () => {
-    cy.get('input.new-todo').type(`${task1}{enter}`);
     cy.get('.todo-list').find('label').contains(task1).should('exist');
     cy.get('.todo-list li').should('have.length', 1)
     cy.get('.clear-completed').click();
@@ -166,7 +182,6 @@ describe('Clear task works only when task is marked as completed ', () => {
   });
 
   it('should type task, verify that task exists, mark task and clear', () => {
-    cy.get('input.new-todo').type(`${task1}{enter}`);
     cy.get('.todo-list').find('label').contains(task1).should('exist');
     cy.get('.todo-list li').should('have.length', 1)
     cy.contains('.todo-list li', task1).find('input.toggle').click()
@@ -176,14 +191,44 @@ describe('Clear task works only when task is marked as completed ', () => {
 
 });
 
+describe('Task List Filter Buttons', () => {
+  beforeEach(() => {
+    cy.visit(pageUrl);
+
+    cy.addTodo(task1);
+    cy.addTodo(task2);
+    cy.addTodo(task3);
+    cy.get('.todo-list li:nth-child(2) .toggle').click();
+  });
+
+  it('should display all tasks when "All" filter is selected', () => {
+    cy.contains('a', 'All').click();
+    cy.get('.todo-list li').should('have.length', 3);
+  });
+
+  it('should display only active taks when "Active" filter is selected', () => {
+    cy.contains('a', 'active').click();
+    cy.get('.todo-list li').should('have.length', 2);
+    cy.get('.todo-list').find('label').contains(task1).should('exist');
+    cy.get('.todo-list').find('label').contains(task2).should('not.exist');
+    cy.get('.todo-list').find('label').contains(task3).should('exist');
+  });
+
+  it('should display only completed task when "Completed" filter is selected', () => {
+    cy.contains('a', 'Completed').click();
+    cy.get('.todo-list li').should('have.length', 1).should('have.class', 'completed');
+    cy.get('.todo-list').find('label').contains(task2).should('exist').should('be.visible');
+  });
+});
+
 describe('Tasks exists after page reload ', () => {
   beforeEach(() => {
     cy.visit(pageUrl);
   });
 
-  it('clear button should not clear not selected task', () => {
-    cy.get('input.new-todo').type(`${task1}{enter}`);
-    cy.get('input.new-todo').type(`${task2}{enter}`);
+  it('should type two tasks, reloasd page and tasks still exists', () => {
+    cy.addTodo(task1);
+    cy.addTodo(task2);
     cy.get('.todo-list').find('label').contains(task1).should('exist');
     cy.get('.todo-list').find('label').contains(task2).should('exist');
     cy.get('.todo-list li').should('have.length', 2)
@@ -192,5 +237,8 @@ describe('Tasks exists after page reload ', () => {
   });
 
 });
+
+
+
 
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,33 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+Cypress.Commands.add('addTodo', (task) => {
+    cy.get('.new-todo').type(`${task}{enter}`);
+});
+
+Cypress.Commands.add('editTask', (task, newTask) => {
+    cy.contains('.todo-list li', task).dblclick();
+    cy.get('.todo-list li.editing .edit').clear().type(`${newTask}{enter}`);
+});
+
+Cypress.Commands.add('verifyTodoCount', () => {
+    cy.get('.todo-count').should('exist');
+    cy.get('.todo-count strong').should('exist');
+    cy.get('.todo-count ng-pluralize').should('exist');
+});
+
+// Verify filter links
+Cypress.Commands.add('verifyFilterLinks', () => {
+    cy.get('.filters').within(() => {
+        cy.get('a').contains('All').should('exist');
+        cy.get('a').contains('active').should('exist');
+        cy.get('a').contains('Completed').should('exist');
+    });
+});
+
+// Verify the clear completed button
+Cypress.Commands.add('verifyClearCompleted', () => {
+    cy.get('.clear-completed').should('exist');
+});
